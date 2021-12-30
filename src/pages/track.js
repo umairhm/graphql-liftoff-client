e15a7abe8c476eead49c468b77724d00e9c520f7
Umair Hafeez <umairhafeez.ca@gmail.com>
2021-12-30 15:42:46 -0500

    initial commit

---
+++ src/pages/track.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { Layout, QueryResult } from '../components';
+import TrackDetail from '../components/track-detail';
+
+/** TRACK gql query to retrieve a single track */
+export const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
+
+const Track = ({ trackId }) => {
+  const { loading, error, data } = useQuery(GET_TRACK, { 
+    variables: { trackId } 
+  });
+
+  return (
+    <Layout>
+      <QueryResult loading={loading} error={error} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Track;
